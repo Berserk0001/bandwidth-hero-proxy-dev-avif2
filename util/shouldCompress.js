0e12efb9d3ab288e75197e54f642ee1249e31ee7
denYOUvo <59727582+denYOUvo@users.noreply.github.com>
2022-11-29 16:43:45 +0700

    Update shouldCompress.js
    avif refactoring

--- util/shouldCompress.js
+++ util/shouldCompress.js
@@ -1,16 +1,17 @@
-//const MIN_COMPRESS_LENGTH = 1024;
-//const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100;
+const MIN_COMPRESS_LENGTH = 2024; // ~2kb
+const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 1; // ~2kb
 
-function shouldCompress(originType, originSize, webp) {
-	if ( !originType.startsWith("image") || originSize === 0) return false;
-//	if (webp && originSize < MIN_COMPRESS_LENGTH) return false;
-//	if (	// if png or gif image, and size is less than compression limit (also shouldn't be webp)
-//		!webp &&
-//		(originType.endsWith("png") || originType.endsWith("gif")) &&
-//		originSize < MIN_TRANSPARENT_COMPRESS_LENGTH
-//	) {
-//		return false;
-//	}
+function shouldCompress(originType, originSize, avif) {
+	if (!originType.startsWith("image")) return false;
+        if (originSize === 0) return false;
+	if (avif && originSize < MIN_COMPRESS_LENGTH) return false;
+	if (	// if png or gif image, and size is less than compression limit (also shouldn't be avif)
+		!avif &&
+		(originType.endsWith("png") || originType.endsWith("gif")) &&
+		originSize < MIN_TRANSPARENT_COMPRESS_LENGTH
+	) {
+		return false;
+	}
 
 	return true;
 }
