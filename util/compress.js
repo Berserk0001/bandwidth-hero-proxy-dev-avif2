ccf16ca0a5d9c745d5d8c11ebef95d1ed8e25fc7
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2023-07-04 15:12:06 +0530

    Update compress.js

--- util/compress.js
+++ util/compress.js
@@ -1,7 +1,7 @@
 const sharp = require("sharp");
 
 function compress(input, avif, grayscale, quality, originSize) {
-	const format = avif ? "avif" : "jpg";
+	const format = avif ? "avif" : "jpeg";
         let compressionQuality = quality * 0.1;
 
         quality = Math.ceil(compressionQuality);
