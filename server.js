5ce6373f6ca799fefcf5d73c9068d1e9b5f0706c
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2023-07-31 10:03:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 #!/usr/bin/env node
 'use strict'
 const app = require('express')()
-const params = require('./src/params')
-//import app from 'express';
-//import authenticate from '../src/authenticate.js';
-//import params from '../src/param.js';
-//import proxy from '../src/proxy.js';
+const index = require('./index')
 
 const PORT = process.env.PORT || 8080
 
-app.get('/' , params)
+app.get('/' , index)
 app.get('/favicon.ico', (req, res) => res.status(204).end())
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
